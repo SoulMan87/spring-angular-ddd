98bad1695dd8bb9622f9f731611e30948e56c893
jonathan <jonathanhinestroza87@gmail.com>
2023-11-09 09:36:44 -0500

    First commit (#1) Spring and Angular project created

---
+++ src/main/java/org/soulrebel/company/Bootstrap.java
@@ -0,0 +1,13 @@
+package org.soulrebel.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run (Bootstrap.class, args);
+    }
+
+}
